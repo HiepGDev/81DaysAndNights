fdb53a1e6916af1bb5d89d89afbfbbd915c9c6ac
HiepHa-24 <hhnh2004@gmail.com>
2026-04-17 03:03:44 +0700

    Added movement animation and bug fixes
    - Add AK47 idle , walk and run animation
    - Fixed the stand up stutter bug
    - Move headbob script to Recoil camera ( was in Player camera )

--- Assets/Scripts/Hiep's Scripts/PlayerMovement.cs
+++ Assets/Scripts/Hiep's Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
     public Transform playerCamera; 
     private CharacterController playerController; 
     InputAction moveAction; 
@@ -32,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float standingHeight = 2.0f; 
     [SerializeField] private float crouchSpeed = 2.0f; 
     [SerializeField] private float crouchTransitionSpeed = 8f; 
+    
     public bool isCrouching = false;
     private Vector3 originalCamPos; 
     // center lerping to stop sinking/stutter
@@ -101,7 +103,13 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.y = -2f;
         }
+        bool isMoving = moveValue.magnitude > 0.1f;
+        bool isWalking = isMoving && !isSprinting;
+        bool isRunning = isMoving && isSprinting;
+        animator.SetBool("isWalk", isWalking);
+        animator.SetBool("isRun", isRunning);
     }
+    
     void HandleLook()
     {
         // Read the mouse delta
@@ -150,10 +158,14 @@ public class PlayerMovement : MonoBehaviour
         } 
         // Smoothly adjust CharacterController height
         float targetHeight = isCrouching ? crouchHeight : standingHeight;
-        playerController.height = Mathf.Lerp(playerController.height, targetHeight, Time.deltaTime * crouchTransitionSpeed);
+        float newHeight = Mathf.Lerp(playerController.height, targetHeight, Time.deltaTime * crouchTransitionSpeed);
+        playerController.height = newHeight;
+
+        playerController.center = new Vector3(0, newHeight / 2f, 0);
+        float heightDifference = standingHeight - newHeight;
 
         // Adjust camera position relative to crouch
-        Vector3 targetCamPos = isCrouching ? new Vector3(originalCamPos.x, originalCamPos.y - 0.5f, originalCamPos.z) : originalCamPos;
+        Vector3 targetCamPos = new Vector3(originalCamPos.x, originalCamPos.y - heightDifference, originalCamPos.z);
         playerCamera.localPosition = Vector3.Lerp(playerCamera.localPosition, targetCamPos, Time.deltaTime * crouchTransitionSpeed);
     }
 }
